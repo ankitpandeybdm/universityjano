d278df6d07f2afacb212e15070d182005ab1aeb6
ankitpandeybdm <ankit.pandeybdm@gmail.com>
2026-09-05 09:45:21 +0530

    Delete check_data.js

--- check_data.js
+++
@@ -1,4 +0,0 @@
-global.window = {};
-require('./data/universities.js');
-console.log('Total universities in data/universities.js:', window.UNIVERSITIES_DATA.length);
-window.UNIVERSITIES_DATA.forEach((u, i) => console.log(`${i+1}. ${u.name} (${u.slug})`));
